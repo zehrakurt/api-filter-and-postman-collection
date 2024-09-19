e35509030a544bf902b292e3192ded5d8ffc07c7
zehrakurt <153917389+zehrakurt@users.noreply.github.com>
2024-09-19 17:47:40 +0300

    .
    .

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
-const PORT = process.env.PORT || 3000; 
+const PORT = 3000;
 const express = require('express');
 const app = express();
-
 const categoryRoutes = require('./src/routes/category');
 const postRoutes = require('./src/routes/post');
 
 app.use(express.json());
 app.use('/categories', categoryRoutes);
-app.use('/post', postRoutes);
+app.use('/posts', postRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
